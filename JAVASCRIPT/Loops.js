cd20988f3375d97e97e35da2578cac6105999907
Daniel Dias <danielantoniomarquesdias21@gmail.com>
2018-03-17 09:53:49 +0000

    t

--- JAVASCRIPT/Loops.js
+++ JAVASCRIPT/Loops.js
@@ -36,3 +36,15 @@ for (let myPlacesIndex = 0; myPlacesIndex < myPlaces.length; myPlacesIndex++) {
        }
   }
 }
+
+//Gerar valores com Math.random
+let cards = ['Diamond', 'Spade', 'Heart', 'Club'];
+
+let currentCard = 'Heart';
+
+while (currentCard !== 'Spade') {
+  console.log(currentCard);
+  currentCard = cards[Math.floor(Math.random() *4)];
+}
+
+console.log('You found a spade!');
